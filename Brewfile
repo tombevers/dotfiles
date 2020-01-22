23e591dabd3842b33dfa2e410b58acd8135a2d69
Tom Bevers <tom.bevers@phpro.be>
2020-01-22 14:41:56 +0100

    Use bres and mackup

---
+++ Brewfile
@@ -0,0 +1,60 @@
+# Taps
+tap 'caskroom/cask'
+tap 'caskroom/eid'
+tap 'caskroom/fonts'
+tap 'caskroom/versions'
+tap 'homebrew/bundle'
+
+# Binaries
+brew 'awscli'
+brew 'bash' # Bash 4
+brew 'bat'
+brew 'coreutils' # Those that come with macOS are outdated
+brew 'git'
+brew 'grep'
+brew 'htop'
+brew 'mackup'
+brew 'mas'
+brew 'trash'
+brew 'tree'
+brew 'tmux'
+brew 'zsh'
+brew 'zsh-completions'
+brew 'openldap'
+brew 'libiconv'
+
+# Development
+brew 'mysql'
+brew 'node'
+brew 'php@7.1'
+brew 'php@7.2'
+brew 'php@7.3'
+brew 'brew-php-switcher'
+
+# Apps
+cask 'firefox'
+cask 'google-chrome'
+cask 'alfred'
+cask 'phpstorm'
+cask '1password'
+cask 'sip'
+cask 'dropbox'
+cask 'sequel-pro'
+cask 'iterm2'
+cask 'spotify'
+cask 'franz'
+cask 'vlc'
+cask 'whatsapp'
+cask 'teamviewer'
+cask 'eid-be'
+cask 'eid-be-viewer'
+cask 'vagrant'
+cask 'java'
+cask 'virtualbox'
+cask 'virtualbox-extension-pack'
+cask 'insomnia'
+cask 'atom'
+
+# Mac App Store
+mas 'Keynote', id: 409183694
+mas 'The Unarchiver', id: 425424353
--- install.sh
+++ install.sh
@@ -4,6 +4,33 @@ dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 files="bashrc vimrc vim zshrc oh-my-zsh"    # list of files/folders to symlink in homedir
 
+echo "Setting up your Mac..."
+
+# Check for Homebrew and install if we don't have it
+if test ! $(which brew); then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update Homebrew recipes
+brew update
+
+# Install all our dependencies with bundle (See Brewfile)
+brew tap homebrew/bundle
+brew bundle
+
+# Make ZSH the default shell environment
+chsh -s $(which zsh)
+
+# Install Oh My ZSH
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Install Composer
+curl -sS https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
+
+# Install global NPM packages
+npm install --global yarn
+
 echo "Installing dotfiles"
 
 # Create dotfiles_old in homedir
@@ -24,3 +51,10 @@ ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/gitignore ~/.gitignore
 echo "...done"
+
+# Symlink the Mackup config file to the home directory
+ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
+
+# Set macOS preferences
+# We will run this last because this will reload the shell
+source .macos
